2eb042ddf08698e2a7b92e1d24cc5eb3ccd140a6
ciehgiordani <acieh_giordani@yahoo.com>
2024-05-19 19:40:05 +0800

    initial commit

---
+++ src/components/ContactForm/ContactForm.styled.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const Input = styled.input`
+  padding:12px;
+  margin-bottom: 12px;
+  border-radius: 6px;
+  border: none;
+  outline: 0;
+  background-color: #fff;
+  box-shadow: 0 1px 6px 0 #20212447;
+`;
+
+export const Button = styled.button`
+  > svg {
+    width: 20px;
+    height: 30px;
+    fill: #02ba52;
+  }
+  /* padding: 10px 20px; */
+  margin-top: 5px;
+  font-size: 14px;
+  border-radius: 6px;
+  border: 1px solid;
+  :hover {
+    background-color: #1677ff;
+    > svg {
+      fill: #fff;
+    }
+  }
+`;
+
+export const Text = styled.p`
+  /* padding: 10px 20px; */
+  margin-bottom: 15px;
+`;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,39 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { Empty } from 'antd';
+import {
+  selectFilteredContacts,
+  selectError,
+  selectIsLoading,
+} from 'redux/selectors';
+import { Loader } from '../Loader/Loader';
+import { fetchContacts } from 'redux/operations';
+import ContactItem from '../ContactItem/ContactItem';
+
+function ContactList() {
+  const filteredContacts = useSelector(selectFilteredContacts);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <ul>
+      {isLoading && !error ? (
+        <Loader />
+      ) : filteredContacts.length === 0 && !error ? (
+        <Empty />
+      ) : (
+        filteredContacts.map(({ id, name, number }) => (
+          <ContactItem key={id} contact={{ id, name, number }} />
+        ))
+      )}
+    </ul>
+  );
+}
+
+export default ContactList;
